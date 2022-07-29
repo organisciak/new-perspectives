71ad164b9ab89b11e61a0cb7f8fa4b24616db9eb
Peter Organisciak <organisciak@gmail.com>
2022-07-29 14:55:20 -0600

    Add cards

--- src/lib/Card.svelte
+++ src/lib/Card.svelte
@@ -2,7 +2,59 @@
 	import { tweened } from 'svelte/motion';
 	import { cubicOut } from 'svelte/easing';
 
-	let msgs = ["go to a new bar", "go to a random restaurant", "change up your route", "drive somewhere new for a run", "cook from a recipe", "wear something you haven't worn in awhile", "take a picture of yourself with a random object", "do a mindfulness meditation", "close your eyes and do a mental walk through a foreign place you've been", "pick up a new hobby", "read a book outside", "lay in the grass and look at the clouds", "have a conversation with a stranger", "take a different form of transportation", "explore a new neighborhood"]
+	let msgs = ["go to a new bar",
+				"go to a random restaurant",
+				"change up your route",
+				"drive somewhere new for a hike or a run",
+				"cook from a recipe",
+				"wear something you haven't worn in awhile",
+				"do a mindfulness meditation",
+				"close your eyes and do a mental walk through a place you haven't been to in a while",
+				"read a book outside",
+				"lay in the grass and look at the clouds",
+				"have a conversation with a stranger",
+				"take a different form of transportation",
+				"explore a new neighborhood",
+				"look up something you've always wanted to know more about",
+				"play a new artist or band",
+				"try a new food",
+				"watch today's sunset",
+				"buy a new plant for your home",
+				"give yourself a break and do something you enjoy",
+				"have a picnic",
+				"go stargazing",
+				"watch a movie you've never seen before",
+				"build something with your hands",
+				"draw or paint something",
+				"plan a trip somewhere you've always wanted to go",
+				"make a list of things you're grateful for",
+				"reach out to someone you haven't talked to in awhile",
+				"take some deep breaths and relax",
+				"go for a walk and take in the sights and sounds around you",
+				"stop and smell the flowers",
+				"listen to the birds singing",
+				"watch the leaves blowing in the wind",
+				"have a seat and people watch",
+				"create something new",
+				"organise your space",
+				"clear out some clutter",
+				"sit by a body of water",
+				"photograph the things you see",
+				"write down your goals",
+				"visit a museum",
+				"go to the zoo",
+				"take a road trip",
+				"plan a day trip",
+				"visit a farmer's market",
+				"go picking for an in-season fruit",
+				"have a picnic in the park",
+				"play tourist in your own town or city",
+				"ride a roller coaster",
+				"play mini golf",
+				"watch a movie in the theater",
+				"go to a comedy club",
+				"go to a planetarium"
+				]
 	let msg_shuffle = msgs.map(value => ({ value, sort: Math.random() }))
 						  .sort((a, b) => a.sort - b.sort)
 						  .map(({ value }) => value)
@@ -49,6 +101,8 @@
 	  </div>
 	</div>
   </div>
+
+  <button on:click="{() => newcard() }">New</button>
 
 <style>
 
