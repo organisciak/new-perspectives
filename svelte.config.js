8fff01b0a996a09d6a5f4dffa93f047e92d0512c
Peter Organisciak <organisciak@gmail.com>
2022-07-29 14:18:51 -0600

    Update adaptor logic

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,7 @@
 import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
-const dev = process.env.NODE_ENV === 'development';
+//const dev = process.env.NODE_ENV === 'development';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -14,8 +14,10 @@ const config = {
 		  fallback: null,
 		  precompress: false,
 		  paths: {
-			  base: dev ? '' : '/new-perspectives',
+			  //base: dev ? '' : '/new-perspectives',
+			  base: '/new-perspectives',
 		  },
+		  appDir: 'internal'
 		}),
 	
 		prerender: {
