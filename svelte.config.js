a1206653a42e950baa3336b4af98cc0c8bfe2646
Peter Organisciak <organisciak@gmail.com>
2022-07-29 14:12:23 -0600

    Fix paths

--- svelte.config.js
+++ svelte.config.js
@@ -1,21 +1,27 @@
 import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
+const dev = process.env.NODE_ENV === 'development';
+
 /** @type {import('@sveltejs/kit').Config} */
-export default {
+const config = {
+	preprocess: preprocess(),
+
 	kit: {
-	  adapter: adapter({
-		// default options are shown. On some platforms
-		// these options are set automatically — see below
-		pages: 'build',
-		assets: 'build',
-		fallback: null,
-		precompress: false
-	  }),
-  
-	  prerender: {
-		// This can be false if you're using a fallback (i.e. SPA mode)
-		default: true
+		adapter: adapter({
+		  pages: 'docs',
+		  assets: 'docs',
+		  fallback: null,
+		  precompress: false,
+		  paths: {
+			  base: dev ? '' : '/new-perspectives',
+		  },
+		}),
+	
+		prerender: {
+		  default: true
+		}
 	  }
-	}
-  };
+};
+
+export default config;
